e5bc25ceb0e5aba50203e50624e91d8a682e6777
Derrick Mercado <33558893+djcmerc@users.noreply.github.com>
2020-02-26 21:14:40 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PA4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wordList = new System.Windows.Forms.ListBox();
+            this.morphBtn = new System.Windows.Forms.Button();
+            this.scrabbleBtn = new System.Windows.Forms.Button();
+            this.rhymeBtn = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.refillList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // wordList
+            // 
+            this.wordList.FormattingEnabled = true;
+            this.wordList.Location = new System.Drawing.Point(12, 12);
+            this.wordList.Name = "wordList";
+            this.wordList.Size = new System.Drawing.Size(287, 420);
+            this.wordList.Sorted = true;
+            this.wordList.TabIndex = 0;
+            this.wordList.Click += new System.EventHandler(this.wordList_Click);
+            this.wordList.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // morphBtn
+            // 
+            this.morphBtn.Location = new System.Drawing.Point(319, 180);
+            this.morphBtn.Name = "morphBtn";
+            this.morphBtn.Size = new System.Drawing.Size(198, 35);
+            this.morphBtn.TabIndex = 1;
+            this.morphBtn.Text = "Find Morph Words";
+            this.morphBtn.UseVisualStyleBackColor = true;
+            this.morphBtn.Click += new System.EventHandler(this.morphBtn_Click);
+            // 
+            // scrabbleBtn
+            // 
+            this.scrabbleBtn.Location = new System.Drawing.Point(319, 121);
+            this.scrabbleBtn.Name = "scrabbleBtn";
+            this.scrabbleBtn.Size = new System.Drawing.Size(198, 35);
+            this.scrabbleBtn.TabIndex = 1;
+            this.scrabbleBtn.Text = "Find Scrabble Words";
+            this.scrabbleBtn.UseVisualStyleBackColor = true;
+            this.scrabbleBtn.Click += new System.EventHandler(this.scrabbleBtn_Click);
+            // 
+            // rhymeBtn
+            // 
+            this.rhymeBtn.Location = new System.Drawing.Point(319, 60);
+            this.rhymeBtn.Name = "rhymeBtn";
+            this.rhymeBtn.Size = new System.Drawing.Size(198, 35);
+            this.rhymeBtn.TabIndex = 1;
+            this.rhymeBtn.Text = "Find Rhyming Words";
+            this.rhymeBtn.UseVisualStyleBackColor = true;
+            this.rhymeBtn.Click += new System.EventHandler(this.rhymeBtn_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(319, 12);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(198, 20);
+            this.textBox.TabIndex = 2;
+            // 
+            // refillList
+            // 
+            this.refillList.Location = new System.Drawing.Point(319, 241);
+            this.refillList.Name = "refillList";
+            this.refillList.Size = new System.Drawing.Size(198, 35);
+            this.refillList.TabIndex = 1;
+            this.refillList.Text = "Refill List";
+            this.refillList.UseVisualStyleBackColor = true;
+            this.refillList.Click += new System.EventHandler(this.refillList_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 448);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.refillList);
+            this.Controls.Add(this.rhymeBtn);
+            this.Controls.Add(this.scrabbleBtn);
+            this.Controls.Add(this.morphBtn);
+            this.Controls.Add(this.wordList);
+            this.Name = "Form1";
+            this.Text = "Word Search";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox wordList;
+        private System.Windows.Forms.Button morphBtn;
+        private System.Windows.Forms.Button scrabbleBtn;
+        private System.Windows.Forms.Button rhymeBtn;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button refillList;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PA4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeListBox();
+        }
+
+        private void InitializeListBox()
+        {
+            String[] word_list = Properties.Resources.WordList.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+            wordList.BeginUpdate();
+            wordList.Items.Clear();
+            foreach (string s in word_list)
+                wordList.Items.Add(s);
+            wordList.EndUpdate();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wordList_Click(object sender, EventArgs e)
+        {
+            textBox.Text = wordList.Text;
+        }
+
+        private void rhymeBtn_Click(object sender, EventArgs e)
+        {
+            InitializeListBox();
+            string endString = textBox.Text;
+            int numRhyming = 0;
+            string tempWord;
+            List<string> rhymingWords = new List<string>();
+
+            // while loop with nested if statement will evaluate the ending letters of the inputted string
+            foreach(string word in wordList.Items)
+            {
+                tempWord = word;
+                if (tempWord.EndsWith(endString) == true)
+                {
+                    rhymingWords.Add(word);
+                    numRhyming++;
+                }
+            }
+            if (numRhyming != 0)
+            {
+                wordList.BeginUpdate();
+                wordList.Items.Clear();
+                foreach (string rhymingWord in rhymingWords)
+                {
+                    wordList.Items.Add(rhymingWord);
+                }
+                wordList.EndUpdate();
+            }
+            else if (numRhyming == 0)
+                MessageBox.Show("No words rhyme with that ending string.");
+        }
+
+        private void morphBtn_Click(object sender, EventArgs e)
+        {
+            InitializeListBox();
+            string morphWord = textBox.Text;
+            string tempWord;
+            int j, numMatches, numMorphWords = 0;
+            List<string> morphWords = new List<string>();
+
+            // Parses the entire list of words
+            foreach(string word in wordList.Items)
+            {
+                numMatches = 0;
+                tempWord = word;
+
+                // For loop compares if current word is one letter different from user inputted word
+                for (j = 0; j < tempWord.Length; j++)
+                {
+                    if (tempWord.Length != morphWord.Length)
+                        break;
+                    else if (tempWord[j] == morphWord[j])
+                        numMatches++;
+                }
+
+                if (numMatches == ((morphWord.Length) - 1))
+                {
+                    morphWords.Add(tempWord);
+                    numMorphWords++;
+                }
+            }
+            if (numMorphWords != 0)
+            {
+                wordList.BeginUpdate();
+                wordList.Items.Clear();
+                foreach(string word in morphWords)
+                {
+                    wordList.Items.Add(word);
+                }
+                wordList.EndUpdate();
+            }
+            else if (numMorphWords == 0)
+                MessageBox.Show("There are no morph words associated with that word. ");
+        }
+
+        private void scrabbleBtn_Click(object sender, EventArgs e)
+        {
+            InitializeListBox();
+            string scrabbleLetters, tempWord, tempScrabble;
+            char[] replace;
+            int j = 0,
+                k = 0,
+                numMatches = 0,
+                numScrabbleWords = 0;
+            List<string> scrabbleWords = new List<string>();
+
+            scrabbleLetters = textBox.Text;
+            if (scrabbleLetters.Length < 3 || scrabbleLetters.Length > 7)
+            {
+                MessageBox.Show("The number of letters must be between 3 and 7. Please try again. ");
+                return;
+            }
+
+
+            // Parses the entire list of words
+            foreach(string word in wordList.Items)
+            {
+                numMatches = 0;
+                tempWord = word;
+                tempScrabble = scrabbleLetters;
+
+                // Compares if the current words can be made from the user inputted letters
+                for (j = 0; j < tempWord.Length; j++)
+                {
+                    for (k = 0; k < tempScrabble.Length; k++)
+                    {
+                        if (tempWord[j] == tempScrabble[k])
+                        {
+                            replace = tempScrabble.ToCharArray();
+                            replace[k] = ' ';
+                            tempScrabble = new string(replace);
+                            numMatches++;
+                            break;
+                        }
+                    }
+                }
+                if ((numMatches == tempWord.Length) && (numMatches > 2))
+                {
+                    scrabbleWords.Add(tempWord);
+                    numScrabbleWords++;
+                }
+            }
+            if (numScrabbleWords != 0)
+            {
+                wordList.BeginUpdate();
+                wordList.Items.Clear();
+                foreach(string word in scrabbleWords)
+                {
+                    wordList.Items.Add(word);
+                }
+                wordList.EndUpdate();
+            }
+            else if (numScrabbleWords == 0)
+                MessageBox.Show("There are no words that can be made from those letters. ");
+        }
+
+        private void refillList_Click(object sender, EventArgs e)
+        {
+            String[] word_list = Properties.Resources.WordList.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+            wordList.BeginUpdate();
+            wordList.Items.Clear();
+            foreach (string s in word_list)
+                wordList.Items.Add(s);
+            wordList.EndUpdate();
+        }
+    }
+}
